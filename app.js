21f420870b011986afde7d615f1fab7316490ada
Lauren Bakke <laurenbakke@laurens-mbp.dhcp.nd.edu>
2020-02-10 11:28:27 -0500

    fixed dropdown

--- app.js
+++ app.js
@@ -20,13 +20,7 @@ angular.module('app').config(function ($routeProvider, $locationProvider) {
 })
 
 // Inline Annotation
-angular.module('app').controller('second', ['$scope', '$http', "ClassService", function ($scope, $http, ClassService) {
-
-    // TODO: MOVE JSON DATA SOURCE TO A SERVICE ()
-    const data = {
-        american: ['pizza', 'burgers', 'hotdogs'],
-        desserts: ['ice cream', 'waffles']
-    }
+angular.module('app').controller('second', ['$scope', "ClassService", function ($scope, ClassService) {
 
     $scope.models = {
         selected: null,
@@ -68,71 +62,19 @@ angular.module('app').controller('second', ['$scope', '$http', "ClassService", f
     };
 }]);
 
-// Explicit dependency injection
-function MainController($scope, $http) {
-    //angular.module('app').controller('MainController', ['$scope', '$http', function ($scope, $http) {
+function MainController($scope) {
 
-    const data = {
-        american: ['pizza', 'burgers', 'hotdogs'],
-        desserts: ['ice cream', 'waffles']
-    }
-    $scope.data = data;
-    $scope.classes = [{
-            name: 'abc',
-            taken: 'abc@gmail.com'
-        },
-        {
-            name: 'xyz',
-            taken: 'xyz@gmail.com'
-        },
-        {
-            name: 'pqr',
-            taken: 'pqr@gmail.com'
-        },
-        {
-            name: 'rs',
-            taken: 'rs@gmail.com'
-        }
-    ];
     const majorOptions = {
         majors: ['Computer Science', 'Computer Engineering']
     }
-    //$scope.majors = majorOptions.majors;
+    $scope.majors = majorOptions.majors;
 
-    // function readTextFile(file, callback) {
-    //     var rawFile = new XMLHttpRequest();
-    //     rawFile.overrideMimeType("application/json");
-    //     rawFile.open("GET", file, true);
-    //     rawFile.onreadystatechange = function () {
-    //         if (rawFile.readyState === 4 && rawFile.status == "200") {
-    //             callback(rawFile.responseText);
-    //         }
-    //     }
-    //     rawFile.send(null);
-    // }
-
-    
-
-    // //usage:
-    // readTextFile("data.json", function (text) {
-    //     var data = JSON.parse(text);
-    //     console.log(data);
-    //     let majorsFound = []
-    //     for (var key in data.major[0]) {
-    //         majorsFound.push(key)
-    //     }
-    //     $scope.majors = majorsFound;
-    //     console.log(majorOptions.majors)
-    // });
-
+    // save which major chosen in dropdown - called on "go" button press
     $scope.saveMajor = function () {
         $scope.majorLocked = $scope.majorChosen;
-        console.log($scope.majorLocked)
     };
-
-
 }
-MainController.$inject = ['$scope', '$http']
+MainController.$inject = ['$scope']
 angular.module('app').controller('MainController', MainController)
 
 function ClassService($http) {
