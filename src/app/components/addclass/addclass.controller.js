0061b030a3891aa5b0730b7015e80a5039d5b82b
Lauren Bakke <laurenbakke@laurens-mbp.dhcp.nd.edu>
2020-02-27 10:58:33 -0500

    working on add class list

--- src/app/components/addclass/addclass.controller.js
+++ src/app/components/addclass/addclass.controller.js
@@ -1,16 +1,30 @@
-function AddClassController($state) {
+function AddClassController($state, $mdDialog, $http) {
     var ctrl = this;
-    console.log("add class controller")
-    ctrl.doSecondaryAction = function(event) {
-        $mdDialog.show(
-          $mdDialog.alert()
-            .title('Secondary Action')
-            .textContent('Secondary actions can be used for one click actions')
-            .ariaLabel('Secondary click demo')
-            .ok('Neat!')
-            .targetEvent(event)
-        );
-      };
+
+    $http.get('../addClass.json').then((JSONdata) => {
+      // all of the json data
+      ctrl.classData = JSONdata;
+      
+      console.log('All Class Data:', JSONdata)
+      
+      // angular.forEach(ctrl.computerScience, function (value, key) {
+      //     ctrl.models.semesters[value.semDefault].push(value.name);
+
+      // });
+      console.log(JSONdata.data.major[0])
+      console.log("add class controller")
+      ctrl.doSecondaryAction = function(event) {
+          $mdDialog.show(
+            $mdDialog.alert()
+              .title("Course Description")
+              .textContent(JSONdata.data.major[0].Economocis.courses[0]['ECON 10011'].description)
+              .ok('Neat!')
+              .targetEvent(event)
+          );
+        };
+
+  })
+
   }
   
 angular
